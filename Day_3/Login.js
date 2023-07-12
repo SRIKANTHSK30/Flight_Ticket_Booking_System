d84a6ad8b076a38eb4e88fe291b2d9227486b005
SRIKANTHSK27 <117260783+SRIKANTHSK27@users.noreply.github.com>
2023-07-12 09:16:47 +0530

    Updated

--- Day_3/Login.js
+++ Day_3/Login.js
@@ -1,68 +1,29 @@
-import React from "react";
-import '../Login/Login.css';
-function Login(){
-    return(
-	<div class="section">
-	<div class="container">
-		<div class="row full-height justify-content-center">
-			<div class="col-12 text-center align-self-center py-5">
-				<div class="section pb-5 pt-5 pt-sm-2 text-center">
-					<h6 className="center"><span>Log In </span><span>Sign Up</span></h6>
-					<div className="nope">
-					</div>
-					<input class="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
-					 
-					  <label for="reg-log"></label>
-					<div class="card-3d-wrap mx-auto">
-						<div class="card-3d-wrapper">
-							<div class="card-front">
-								<div class="center-wrap">
-									<div class="section text-center">
-										<h4 class="mb-4 pb-3">Log In</h4>
-										<div class="form-group">
-											<input type="email" name="logemail" class="form-style" placeholder="Your Email" id="logemail" autocomplete="off"/>
-											<i class="input-icon uil uil-at"></i>
-										</div><br></br>
-										<div class="form-group mt-2">
-											<input type="password" name="logpass" class="form-style" placeholder="Your Password" id="logpass" autocomplete="off"/>
-											<i class="input-icon uil uil-lock-alt"></i>
-										</div>
-										<br>
-										</br>
-										<a href="#" class="btn mt-4">Submit</a>
-										<p class="mb-0 mt-4 text-center"><a href="#0" class="link">Forgot your password?</a></p>
-									  </div>
-								  </div>
-							  </div>
-							<div class="card-back">
-								<div class="center-wrap">
-									<div class="section text-center">
-										<h4 class="mb-4 pb-3">Sign Up</h4>
-										<div class="form-group">
-											<input type="text" name="logname" class="form-style" placeholder="Your Full Name" id="logname" autocomplete="off"/>
-											<i class="input-icon uil uil-user"></i>
-										</div><br></br>
-										<div class="form-group mt-2">
-											<input type="email" name="logemail" class="form-style" placeholder="Your Email" id="logemail" autocomplete="off"/>
-											<i class="input-icon uil uil-at"></i>
-										</div>	<br></br>
-										<div class="form-group mt-2">
-											<input type="password" name="logpass" class="form-style" placeholder="Your Password" id="logpass" autocomplete="off"/>
-											<i class="input-icon uil uil-lock-alt"></i>
-										</div>
-										<br>
-										</br>
-										<a href="#" class="btn mt-4">submit</a>
-									  </div>
-								  </div>
-							  </div>
-						  </div>
-					  </div>
-				  </div>
-			  </div>
-		  </div>
-	</div>
-</div>
-    );
+import "../Flight/Login.css";
+
+function Login() {
+  return (
+    <form className="login__form" method="POST" name="frm_login" action="#">
+    <header className="login__header">
+       <h1 className="login__title">Login</h1>
+    </header>
+    <main className="login__main">
+       <div className="login__group">
+          <input className="login__input" type="text" name="input_email" required/>
+          <label className="login__label">Email </label>
+          <div className="login__bar"></div>
+       </div>
+       <div className="login__group">
+          <input className="login__input" type="password" name="input_password" required/>
+          <label className="login__label">Password </label>
+          <div className="login__bar"></div>
+       </div>
+       <p className="login__terms">Create an Account ?<a href="#">SIGN UP</a></p>
+    </main>
+    <footer className="login__footer">
+      <input className="login__button" type="button" name="btn_signin" value="Login"/>
+    </footer>
+ </form>
+  );
 }
+
 export default Login;
